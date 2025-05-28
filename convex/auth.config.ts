14873415b098065b1662d3728e6516bf5d71082f
Binit <binitdeb5396@gmail.com>
2025-05-29 01:01:05 +0530

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://eternal-gazelle-98.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
